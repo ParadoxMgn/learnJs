79f6b2f7ad60276026b70147cf99abe2fc57401e
Eugene Shumkov <radoxmgn@gmail.com>
2022-05-30 18:04:15 +0300

    Task of the lesson03

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 'use strict';
 
-const title = 'Name Project';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 777;
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+const screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 const rollback = 15;
-const fullPrice = 55999;
-const adaptive = false;
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?');
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)).toFixed(2));
 
 console.log(typeof (title));
 console.log(typeof (fullPrice));
@@ -15,3 +20,15 @@ console.log('Стоимость верстки экранов ' + screenPrice + 
 console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
 console.log(screens.toLowerCase().split(", "));
 console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback / 100)).toFixed(2));
+
+console.log('Итоговая стоимость ' + servicePercentPrice);
+
+if (fullPrice > 30000) {
+  console.log('Даем скидку в 10%');
+} else if (fullPrice > 15000 && fullPrice <= 30000) {
+  console.log('Даем скидку в 5%');
+} else if (fullPrice >= 0 && fullPrice <= 15000) {
+  console.log('Скидка не предусмотрена');
+} else if (fullPrice < 0) {
+  console.log('Что то пошло не так');
+}
