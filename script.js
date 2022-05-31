b2c610915cc7f1fb785666a09ad1b9286e2eab27
Eugene Shumkov <radoxmgn@gmail.com>
2022-05-31 20:12:36 +0300

    Task of the lesson04

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
 'use strict';
 
-const title = 'Name Project';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 777;
+let title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+const screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 const rollback = 15;
-const fullPrice = 55999;
-const adaptive = false;
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?');
 
-console.log(typeof (title));
-console.log(typeof (fullPrice));
-console.log(typeof (adaptive));
-console.log(screens.length);
-console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
-console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
-console.log(screens.toLowerCase().split(", "));
-console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback / 100)).toFixed(2));
+const allServicePrices = function (onePrice, twoPrice) {
+  return onePrice + twoPrice;
+};
+
+function getFullPrice(someScreenPrice, allServPrice) {
+  return someScreenPrice + allServPrice;
+}
+
+function getTitle(correctTitle) {
+  const cutTitle = correctTitle.trim();
+  return cutTitle[0].toUpperCase() + cutTitle.toLowerCase().slice(1);
+}
+
+function getServicePercentPrices(someFullPrice, someRollBack) {
+  return "Стоимость за вычетом процента отката посреднику: " + Math.ceil(someFullPrice - (someFullPrice * (someRollBack / 100)).toFixed(2));
+}
+
+function showTypeOf(param) {
+  return param + " " + typeof param;
+}
+
+function getRollbackMessage(price) {
+  if (price > 30000) {
+    return 'Даем скидку в 10%';
+  } else if (price > 15000 && price <= 30000) {
+    return 'Даем скидку в 5%';
+  } else if (price >= 0 && price <= 15000) {
+    return 'Скидка не предусмотрена';
+  } else if (price < 0) {
+    return 'Что то пошло не так';
+  }
+}
+
+title = getTitle(title);
+const fullPrice = getFullPrice(screenPrice, allServicePrices(servicePrice1, servicePrice2));
+const servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+
+console.log(showTypeOf(title));
+console.log(showTypeOf(fullPrice));
+console.log(showTypeOf(adaptive));
+console.log(screens);
+console.log(getRollbackMessage(fullPrice));
+console.log(servicePercentPrice);
